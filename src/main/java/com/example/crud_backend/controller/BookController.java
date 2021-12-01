fc0212ac9795d04f8443e0b77d0f60aa0c6bf9c9
Milindi Senevirathne <milindianu@gmail.com>
2021-12-01 13:55:53 +0530

    fifth commit

--- src/main/java/com/example/crud_backend/controller/BookController.java
+++ src/main/java/com/example/crud_backend/controller/BookController.java
@@ -24,9 +24,9 @@ import java.util.List;
 @RequestMapping("api/v1/books")
 public class BookController {
 
-    @Autowired
     private final BookService bookService;
 
+    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
@@ -37,37 +37,47 @@ public class BookController {
     }
 
     @GetMapping(path = "{id}")
-    public Book getBook(@PathVariable("id") Long id){return bookService.getBook(id);}
+    public Book getBook(@PathVariable("id") Long id) {
+        return bookService.getBook(id);
+    }
+
+    @GetMapping(path = "invoice/{id}")
+    public ResponseEntity<Resource> getFile(@PathVariable("id") Long id) {
+        Resource file = bookService.getFile(id);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
 
     // build create book REST API
-    @PostMapping
-    public Book addNewBook(@RequestParam("file") MultipartFile file,
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void addNewBook(@RequestParam("file") MultipartFile file,
                            @RequestParam("bookName") String bookName,
                            @RequestParam("authorName") String authorName,
                            @RequestParam("quantity") int quantity,
-                           @RequestParam("price") double price){
+                           @RequestParam("price") Double price) throws IOException {
 
-        return bookService.addBook(file, bookName, authorName, quantity, price);
+        bookService.addBook(file, bookName, authorName, quantity, price);
 
     }
 
     // build update book REST API
     @PutMapping(path = "{bookId}")
     public void updateBook(
-            @PathVariable("bookId") Long id,
-            @RequestParam(name="file" , required=false)MultipartFile file,
-            @RequestParam(name = "bookName",required = false) String bookName,
-            @RequestParam(name = "authorName",required = false) String authorName,
-            @RequestParam(name = "quantity",required = false) int quantity,
-            @RequestParam(name = "price",required = false) double price){
-        bookService.updateBook(file, id, bookName,authorName,price,quantity);
+            @PathVariable(name = "bookId") Long bookId,
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            @RequestParam(name = "bookName", required = false) String bookName,
+            @RequestParam(name = "authorName", required = false) String authorName,
+            @RequestParam(name = "quantity", required = false) int quantity,
+            @RequestParam(name = "price", required = false) Double price) {
+        bookService.updateBook(file, bookId, bookName, authorName, price, quantity);
     }
 
     // build delete book REST API
     @DeleteMapping(path = "{bookId}")
-    public void deleteBook(@PathVariable("bookId") Long id){
-        bookService.deleteBookById(id);
+    public void deleteBook(@PathVariable("bookId") Long bookId) {
+        bookService.deleteBookById(bookId);
 
     }
 }
-
--- src/main/java/com/example/crud_backend/entity/Book.java
+++ src/main/java/com/example/crud_backend/entity/Book.java
@@ -6,29 +6,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-
 @Getter
 @Setter
 //@Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Book {
     @Id
-    @SequenceGenerator(
-            name = "book_sequence",
-            sequenceName = "book_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "book_sequence"
-    )
 
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String bookName;
     private String authorName;
     private int quantity;
-    private double price;
+    private Double price;
     private String invoicePath;
+
+    public Book(String bookName, String authorName, int quantity,Double price,String invoicePath) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.quantity=quantity;
+        this.price = price;
+        this.invoicePath=invoicePath;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + bookName + '\'' +
+                ", author='" + authorName + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", invoice='" + invoicePath + '\'' +
+                '}';
+    }
 }
